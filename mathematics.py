f3f701cf4eb2a8395475e67dae770eb6eda296d3
Kunyelewosi Alexander <kunalexander360@gmail.com>
2024-03-20 11:55:54 +0000

    Mean angle

--- mathematics.py
+++ mathematics.py
@@ -1,4 +1,4 @@
-import math
+from math import sin, cos, degrees, radians, atan2
 
 """
 x = 50
@@ -8,10 +8,37 @@ c = exp(x)
 print(a)
 """
 
-angles = [0, 30, 45, 60, 90, -90]
-sines = [math.sin(math.radians(angle)) for angle in angles]
+# angles = [0, 30, 45, 60, 90, -90]
+# sines = [math.sin(math.radians(angle)) for angle in angles]
 # sines = [round(sine, 3) for sine in sines]
-print(sines)
+# print(sines)
 
-angles = [math.degrees(math.asin(sine)) for sine in sines]
-print(angles)
+# angles = [math.degrees(math.asin(sine)) for sine in sines]
+
+
+# print(angles)
+
+# Mean Angle
+
+def meanAngle(angles, inDegrees=True):
+    sumCos = 0.0
+    sumSin = 0.0
+
+    for angle in angles:
+        if inDegrees:
+            angle = radians(angle)
+        sumCos += cos(angle)
+        sumSin = sin(angle)
+
+    N = len(angles)
+    meanAng = atan2(sumSin / N, sumCos / N)
+
+    if inDegrees:
+        meanAng = degrees(meanAng)
+
+    return meanAng
+
+
+angles2 = [100, 150, 150, 70, 90, 160]
+meanAng = meanAngle(angles2)
+print(meanAng)
