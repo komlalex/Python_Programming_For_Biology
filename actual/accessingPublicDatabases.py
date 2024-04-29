c4ec3f4b8af4b69173b3b2ba08b74f805f090bfe
Kunyelewosi Alexander <kunalexander360@gmail.com>
2024-04-29 02:59:16 +0000

    SWISSPROT

--- actual/accessingPublicDatabases.py
+++ actual/accessingPublicDatabases.py
@@ -1,4 +1,4 @@
-from Bio import Entrez, SeqIO
+from Bio import Entrez, SeqIO, ExPASy
 
 Entrez.email = "kunalexander360@gmail.com"
 handle = Entrez.einfo()
@@ -7,25 +7,26 @@ rec = Entrez.read(handle)
 handle2 = Entrez.esearch(db="nucleotide", term="CRT[Gene Name] AND Plasmodium falciparum[Organism]", retmax="40")
 rec_list = Entrez.read(handle2)
 handle2.close()
-#print(rec_list.keys())
-#print(rec_list["Count"])
+# print(rec_list.keys())
+# print(rec_list["Count"])
 print(rec_list["IdList"])
 print(len(rec_list["IdList"]))
 
 idList = rec_list["IdList"]
-handle3 = Entrez.efetch(db="nucleotide", id = idList, rettype="gb")
+handle3 = Entrez.efetch(db="nucleotide", id=idList, rettype="gb")
 recs = list(SeqIO.parse(handle3, "gb"))
 handle3.close()
 
-
 for rec in recs:
-        print(rec.name)
-        print(rec.id)
-        print(rec.description)
-        print(rec.seq)
-        break
+    print(rec.name)
+    print(rec.id)
+    print(rec.description)
+    print(rec.seq)
+    break
 
-
-
-
-
+socketObj = ExPASy.get_sprot_raw("HBB_HUMAN")
+proteinObj = SeqIO.read(socketObj, "swiss")
+print("This HBB!!!")
+print(proteinObj.description)
+print(proteinObj.seq)
+socketObj.close()
